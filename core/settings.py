e8c14c43fa86c1d1476e8dd1122af62aa0961f74
Camilo Morales <camiloandres.1509@gmail.com>
2021-02-01 15:05:44 -0500

    Modifying allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=False)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'techstars-matchmaking.herokuapp.com/', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'techstars-matchmaking.herokuapp.com', config('SERVER', default='127.0.0.1')]
 
 # Application definition
 
