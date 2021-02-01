0cc281bfad13b0f3229be5ae1f8bcd7031ebf63c
Camilo Morales <camiloandres.1509@gmail.com>
2021-02-01 16:38:28 -0500

    Updating

--- core/settings.py
+++ core/settings.py
@@ -72,6 +72,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
